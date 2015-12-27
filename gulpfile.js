bf1c9bedd3252c05ef7a95188c17c84376ad2a16
Jeroen Ooms <jeroen.ooms@incentro.com>
2015-12-27 08:51:33 +0100

    Add typography

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,27 @@ var params = $.util.env;
 var config = {
     src: {
         base: 'src',
-        images: 'src/images',
         scripts: 'src/scripts',
         styles: 'src/styles'
     },
     dist: {
         base: 'dist',
-        images: 'dist/images',
-        scripts: 'dist/scripts',
-        styles: 'dist/styles'
+    },
+    demo: {
+        base: 'dist/demo',
+        scripts: 'dist/demo/scripts',
+        styles: 'dist/demo/styles'
     },
     autoprefixer: ['last 2 versions', 'Explorer >= 8', 'Firefox >= 25']
 };
 
 gulp.task('styles', function () {
+
+    var filter = $.filter(['initialize.*','!*.map']);
+
     return $.rubySass(config.src.styles + '/*.scss', {
             precision: 10,
-            sourcemap: params.production ? false : true,
+            sourcemap: false,
             style: params.production ? 'compressed' : 'expanded',
             loadPath: ['node_modules']
         })
@@ -35,36 +39,32 @@ gulp.task('styles', function () {
         .pipe($.plumber())
         .pipe($.autoprefixer(config.autoprefixer))
         .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest(config.dist.styles))
+        .pipe(gulp.dest(config.demo.styles))
+        .pipe(filter)
+        .pipe(gulp.dest(config.dist.base))
+        .pipe($.cssmin())
+        .pipe($.rename({suffix: '.min'}))
+        .pipe(gulp.dest(config.dist.base))
         .pipe($.size({title: 'styles'}));
 });
 
-gulp.task('images', function () {
-    return gulp.src([config.src.images + '/**/*'])
-        .pipe($.plumber())
-        .pipe(gulp.dest(config.dist.scripts))
-        .pipe($.size({title: 'images'}));
-});
+gulp.task('copy', function () {
 
-gulp.task('scripts', function () {
-    return gulp.src([config.src.scripts + '/**/*.js'])
+    var filter = $.filter(['initialize.scss']);
+
+    return gulp.src(config.src.styles + '/**/initialize.*')
         .pipe($.plumber())
-        .pipe(params.production ? $.uglify() : $.util.noop())
-        .pipe(gulp.dest(config.dist.scripts))
-        .pipe($.size({title: 'scripts'}));
+        .pipe(gulp.dest(config.dist.base))
+        .pipe(filter)
+        .pipe($.rename({basename: 'initialize'}))
+        .pipe(gulp.dest(config.dist.base))
+        .pipe($.size({title: 'copy'}));
 });
 
 gulp.task('html', function () {
     return gulp.src([config.src.base + '/**/*.html'])
-        .pipe(gulp.dest(config.dist.base))
+        .pipe(gulp.dest(config.demo.base))
         .pipe($.size({title: 'html'}));
-});
-
-gulp.task('jshint', function () {
-    gulp.src([config.src.scripts + '/**/*.js'])
-        .pipe($.plumber())
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('clean', function (cb) {
@@ -75,7 +75,7 @@ gulp.task('watch', ['build'], function () {
     browserSync.init({
         reloadDelay: 100,
         server: {
-            baseDir: './' + config.dist.base
+            baseDir: './' + config.demo.base
         }
     });
 
@@ -84,10 +84,10 @@ gulp.task('watch', ['build'], function () {
     gulp.watch([config.src.scripts + '/**/*.js'], ['scripts']);
     gulp.watch([config.src.base + '/**/*.html'], ['html']);
 
-    gulp.watch(config.dist.base + '/**/*').on('change', browserSync.reload);
+    gulp.watch([config.demo.base + '/**/*', config.dist.base + '/**/*']).on('change', browserSync.reload);
 });
 
-gulp.task('build', ['jshint', 'scripts', 'images', 'styles', 'html']);
+gulp.task('build', ['styles', 'html', 'copy']);
 
 gulp.task('deploy', ['build'], function() {
     params.message = params.m || params.message;
@@ -95,7 +95,7 @@ gulp.task('deploy', ['build'], function() {
     var options = {};
     options.message = params.message || 'Update ' + new Date();
 
-    return gulp.src(config.dist.base + '/**/*')
+    return gulp.src(config.demo.base + '/**/*')
         .pipe($.ghPages(options));
 });
 
