2f5464f4908330521b77aa92b047c11817799209
Kasper <kasperreijnders@gmail.com>
2017-10-31 21:47:07 +0100

    fixed build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var rimraf = require('rimraf');
-var browserSync = require('browser-sync');
-var params = $.util.env;
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const rimraf = require('rimraf');
+const browserSync = require('browser-sync');
+const params = $.util.env;
 
-var config = {
+const config = {
     src: {
         base: 'src',
         scripts: 'src/scripts',
@@ -23,19 +23,31 @@ var config = {
     autoprefixer: ['last 2 versions', 'Explorer >= 8', 'Firefox >= 25']
 };
 
-gulp.task('styles', function () {
+gulp.task('styles', () => {
 
-    var filter = $.filter(['initialize.*','!*.map']);
+    // const filter = $.filter(['initialize.**', '!*.map']);
 
     return gulp.src(config.src.styles + '/*.scss')
         .pipe($.sass({
-            precision: 10
+            precision: 10,
+            sourceMap: true
         }))
         .pipe($.plumber())
         .pipe($.autoprefixer(config.autoprefixer))
         .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest(config.demo.styles))
-        .pipe(filter)
+        .pipe(gulp.dest(config.demo.styles));
+        // .pipe(filter)
+        // .pipe(gulp.dest(config.dist.base))
+        // .pipe($.cssmin())
+        // .pipe($.rename({suffix: '.min'}))
+        // .pipe(gulp.dest(config.dist.base))
+        // .pipe($.size({title: 'styles'}));
+});
+
+// @TODO: the filter function does not seem to work...
+gulp.task('styles.copy', ['styles'], () => {
+
+    return gulp.src(config.demo.styles + '/initialize.css')
         .pipe(gulp.dest(config.dist.base))
         .pipe($.cssmin())
         .pipe($.rename({suffix: '.min'}))
@@ -43,24 +55,24 @@ gulp.task('styles', function () {
         .pipe($.size({title: 'styles'}));
 });
 
-gulp.task('copy', function () {
+gulp.task('copy', () => {
     return gulp.src(config.src.styles + '/initialize.*')
         .pipe($.if('initialize.scss', $.rename({basename: '_initialize'})))
         .pipe(gulp.dest(config.dist.base))
         .pipe($.size({title: 'copy'}));
 });
 
-gulp.task('html', function () {
+gulp.task('html', () => {
     return gulp.src([config.src.base + '/**/*.html'])
         .pipe(gulp.dest(config.demo.base))
         .pipe($.size({title: 'html'}));
 });
 
-gulp.task('clean', function (cb) {
+gulp.task('clean', (cb) => {
     rimraf(config.dist.base, cb);
 });
 
-gulp.task('watch', ['build'], function () {
+gulp.task('watch', ['build'], () => {
     browserSync.init({
         server: {
             baseDir: './' + config.demo.base
@@ -68,19 +80,19 @@ gulp.task('watch', ['build'], function () {
     });
 
     gulp.watch([config.src.images + '/**/*'], ['images']);
-    gulp.watch([config.src.styles + '/**/*.scss'], ['styles']);
+    gulp.watch([config.src.styles + '/**/*.scss'], ['styles.copy']);
     gulp.watch([config.src.scripts + '/**/*.js'], ['scripts']);
     gulp.watch([config.src.base + '/**/*.html'], ['html']);
 
     browserSync.watch([config.demo.base + '/**/*', config.dist.base + '/**/*']).on('change', browserSync.reload);
 });
 
-gulp.task('build', ['styles', 'html', 'copy']);
+gulp.task('build', ['styles.copy', 'html', 'copy']);
 
-gulp.task('deploy', ['build'], function() {
+gulp.task('deploy', ['build'], () => {
     params.message = params.m || params.message;
 
-    var options = {};
+    const options = {};
     options.message = params.message || 'Update ' + new Date();
 
     return gulp.src(config.demo.base + '/**/*')
